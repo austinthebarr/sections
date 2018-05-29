b593da4e09d11e228c9a68562a455e2eb7a9db1b
Austin Barr and Matt Staudacher <matt@>
2018-05-29 15:11:39 -0700

    hid sections

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".clickable").click(function(){
+    $(".tesla").slideToggle()
+    $(".tesla2") .slideToggle()
+  });
+
+    $(".hurt").click(function(){
+    $(".js-hell").fadeToggle()
+    $(".js-hidden").fadeToggle()
+  });
+});
